ee6479668c5a40d364560f90dbdd5a299b0f2ff6
Rob Freiburger <rob@robfreiburger.com>
2013-08-08 20:11:40 -0700

    Starts implementation of FizzBuzz class.

--- fizzbuzz/lib/fizz_buzz.rb
+++ fizzbuzz/lib/fizz_buzz.rb
@@ -1,10 +1,50 @@
 class FizzBuzz
 
-	def initialize(n)
+	def initialize(count)
+		if count.is_a? Integer && count >= 1
+			@count = count
+		end
+		@results = []
+		calculate
+	end
+
+	def is_fizz(number)
+		number % 3 == 0
+	end
+
+	def is_buzz(number)
+		number % 5 == 0
+	end
+
+	def at(count)
+		@results[count - 1].to_s
 	end
 
 	def play
-		''
+		@results.join(',')
+	end
+
+	def calculate
+		if @count
+
+			1.upto @count do |i|
+				result = nil
+
+				if is_fizz(i)
+					result += 'fizz'
+				end
+
+				if is_buzz(i)
+					result += 'buzz'
+				end
+
+				if result.nil?
+					result = i.to_s
+				end
+
+				@results << result
+			end
+		end
 	end
 
 end
